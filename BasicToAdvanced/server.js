d3b147907796872d64c4405266d071e23b12f378
Paras Rawat <rawatparas776@gmail.com>
2026-02-19 18:55:15 +0530

    backend-from-scratch

--- BasicToAdvanced/server.js
+++ BasicToAdvanced/server.js
@@ -1,5 +1,6 @@
 const app = require("./src/app")
 const db = require("./src/db/db")
+require('dotenv').config();
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000")
--- BasicToAdvanced/src/db/db.js
+++ BasicToAdvanced/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectDB(){
-    await mongoose.connect ("mongodb+srv://paras:123@learning-mongoose.sjqn9ge.mongodb.net/learning")
+    await mongoose.connect (process.env.MONGO_URI)
     console.log("connected to database")
 }
 
